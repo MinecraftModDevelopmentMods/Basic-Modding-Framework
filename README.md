7268494b6e655c9d086e52f9527fa87821672cfd
ProxyNeko <proxyneko@users.noreply.github.com>
2019-10-24 23:05:20 +0100

    Initial commit for 1.12.2

---
+++ README.md
@@ -0,0 +1,9 @@
+[![](https://img.shields.io/badge/Discord-MMD-green.svg?style=flat&logo=Discord)](https://discord.mcmoddev.com)  
+
+# Basic Modding Framework  
+Remember to edit the following files to provide your mods information.  
+settings.gradle  
+update.json  
+README.md  
+LICENSE.md  
+CHANGELOG.txt  
---
+++ src/main/java/com/mcmoddev/examplemod/ExampleMainClass.java
@@ -0,0 +1,33 @@
+package com.mcmoddev.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nonnull;
+
+@Mod(
+        modid = ExampleMainClass.MODID,
+        name = ExampleMainClass.NAME,
+        version = ExampleMainClass.VERSION,
+        updateJSON = ExampleMainClass.UPDATE_JSON,
+        certificateFingerprint = "@FINGERPRINT@",
+        acceptedMinecraftVersions = ExampleMainClass.MC_VERSION
+)
+public class ExampleMainClass {
+
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "0.0.0";
+    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/MinecraftModDevelopmentMods/examplemod/master/update.json";
+    public static final String MC_VERSION = "[1.12, 1.12.2]";
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
+
+    @Mod.EventHandler
+    public void onFingerprintViolation(@Nonnull FMLFingerprintViolationEvent event) {
+        LOGGER.warn("Invalid fingerprint detected! The file " + event.getSource().getName() +
+                " may have been tampered with. This version will NOT be supported! Please download the mod from " +
+                "CurseForge for a supported and signed version of the mod.");
+    }
+}
